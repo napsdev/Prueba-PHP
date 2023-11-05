2cc138561a7ec9bdf5e89677592ce702d606d824
Jaime Valencia <jaime.valencia@salesdecameron.com>
2023-11-05 16:57:56 -0500

    Sintaxis básica de PHP

---
+++ sintaxis.php
@@ -0,0 +1,24 @@
+<?php // Declara que es codigo PHP.
+
+//Siempre acabar con ; al final de cada linea de codigo PHP.
+echo "Hola mundo! \n"; 
+// \n es un salto de linea.
+// Para navegador es <br>
+
+echo "<br Hola mundo! <br> Soy Jaime!";
+
+//Concatenar en php es con .
+$name = "Napa";
+$lasname = "Sham";
+
+echo "<br> Mas conocido como: <br>" . $name . "<br>" . $lasname;
+
+//Concatenar usando las variables dentro de las comillas dobles
+echo "<br> $name!!! <br> $lasname!!!";
+
+//Operaciones matematicas
+echo "<br> Numerito: " . (2 + 2) * 2;
+
+
+//Cierra el bloque de código PHP, si solo se usara codigo PHP no es necesario cerrarlo. 
+?> 
